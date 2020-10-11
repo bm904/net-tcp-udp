8e64cca42d45dd36ae295f977791699ad08e3b2e
Pierrot <bm904@hotmail.com>
2020-10-11 12:07:53 +0200

    update udp server

--- src/udp.js
+++ src/udp.js
@@ -5,5 +5,4 @@ socket.on('message', (msg, rinfo) => {
   console.log(`server received the message : ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
-socket.bind(8081);
-console.log("Starting server UDP on 8081")
+socket.bind(8081, () => console.log("Starting server UDP on 8081"));
